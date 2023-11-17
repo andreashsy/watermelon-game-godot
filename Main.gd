206850dd5c933267541055d7d902516f875b7c1e
unknown <songyanghan+git@gmail.com>
2023-11-18 01:01:21 +0800

    add random sprite generation for testing purposes

--- Main.gd
+++ Main.gd
@@ -4,11 +4,21 @@ var circle = preload("res://Circle.tscn")
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	var circle_instance = circle.instance()
-	circle_instance.position = Vector2(800, 0)
-	
-	add_child(circle_instance)
+	generate_random(400, 800, circle, 20, 0.5)
 
+func generate_random(x_min: int, x_max: int, obj, num_times: int, delay: float):
+	randomize()
+	for n in num_times:
+		print("Loop " + str(n) + " of " + str(num_times))
+		yield(wait(delay), "completed")
+		
+		var obj_instance = obj.instance()
+		obj_instance.position = Vector2(rand_range(x_min, x_max), 0)
+		
+		add_child(obj_instance)
+
+func wait(seconds: float):
+	yield(get_tree().create_timer(seconds), "timeout")
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _process(delta):
