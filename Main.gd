ef2fb1aede658d2bf1696f787a8858fd914cf03a
unknown <songyanghan+git@gmail.com>
2023-11-19 11:57:43 +0800

    rename circle to yellow smiley face

--- Main.gd
+++ Main.gd
@@ -1,6 +1,6 @@
 extends Node2D
 
-var circle = preload("res://Circle.tscn")
+var circle = preload("res://YellowSmileyFace.tscn")
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
