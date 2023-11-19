6ae310cf4c5645b78f21cde7349d15081ab0a3b9
unknown <songyanghan+git@gmail.com>
2023-11-19 12:21:49 +0800

    add cloud movement

---
+++ Cloud.gd
@@ -0,0 +1,18 @@
+extends RigidBody2D
+
+var velocity = Vector2.ZERO
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+func _physics_process(delta):
+	if Input.is_action_pressed("ui_left"):
+		velocity.x -= 40
+	elif Input.is_action_pressed("ui_right"):
+		velocity.x += 40
+	
+	velocity.x = lerp(velocity.x, 0, 0.1)
+	position += velocity * delta
+	
+	position.x = clamp(position.x, 500, 900)
