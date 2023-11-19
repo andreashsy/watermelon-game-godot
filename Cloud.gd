5200288c4fed432901c95ddc17a99282afb31ee9
unknown <songyanghan+git@gmail.com>
2023-11-19 13:30:10 +0800

    add spawning face and dropping faces

--- Cloud.gd
+++ Cloud.gd
@@ -18,6 +18,23 @@ func _physics_process(delta):
 	
 	position.x = clamp(position.x, 500, 850)
 	
-	if Input.is_action_pressed("ui_up"):
-		var obj_instance = circle.instance()
-		add_child(obj_instance)
+	if Input.is_action_just_pressed("ui_up"):
+		if get_child_count() == 2:
+			var obj_instance = circle.instance()
+			obj_instance.get_node("RigidBody2DYellowSmileyFace").gravity_scale = 0
+			add_child(obj_instance)
+		else:
+			print("node already present")
+			
+	if Input.is_action_just_pressed("ui_down"):
+		if get_child_count() > 2:
+			var child_node = get_child(2)
+			
+			print("Got child: " + str(child_node.name))
+			
+			var grandparent_node = get_parent()
+			remove_child(child_node)
+			grandparent_node.add_child(child_node)
+			child_node.get_node("RigidBody2DYellowSmileyFace").gravity_scale = 1
+			child_node.position = position
+			
