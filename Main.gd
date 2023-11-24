65d7bd0b06a1cb7e47b4da737b53c60f00eb068b
unknown <songyanghan+git@gmail.com>
2023-11-24 20:42:33 +0800

    fix game crash due to null cloud node

--- Main.gd
+++ Main.gd
@@ -67,5 +67,6 @@ func _on_TimerActivateNoTouchZone_timeout():
 
 func _on_NoTouchZone_body_entered(_body:Node):
 	print("Game over")
-	$Cloud.queue_free()
+	if $Cloud:
+		$Cloud.queue_free()
 	$TimerActivateNoTouchZone.stop()
